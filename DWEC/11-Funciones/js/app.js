b15eba88e1761189d88b77b9d484f5291d0e5725
Julieta <jlancellottii23@fpcoruna.afundacion.org>
2024-11-13 10:09:33 +0100

    FOREACH

--- DWEC/11-Funciones/js/app.js
+++ DWEC/11-Funciones/js/app.js
@@ -52,30 +52,30 @@
 //devuelve: el numero de pares es : 
 
 
-function cuentaPares(arraydenums){
-    let totalpares=0, totalimpares=0;
-    if (Array.isArray(arraydenums)){      //SI ES ARRAY
-        for(let num of arraydenums){
-            if (typeof num === 'number'){  //SI ES NUMERO
-                if(num % 2 == 0){    // SI ES PAR
-                    totalpares++;
-                }
-                else{
-                    totalimpares++;
-                }
-            }else{//SI NO ES NUMERO
-                return `El numero ${num} no es número como tal`;
-                break;
-            }
-        }
-    }else{
-        return `No es array`;
-    }
-    return `El numero de pares es ${totalpares}`;
-}
+// function cuentaPares(arraydenums){
+//     let totalpares=0, totalimpares=0;
+//     if (Array.isArray(arraydenums)){      //SI ES ARRAY
+//         for(let num of arraydenums){
+//             if (typeof num === 'number'){  //SI ES NUMERO
+//                 if(num % 2 == 0){    // SI ES PAR
+//                     totalpares++;
+//                 }
+//                 else{
+//                     totalimpares++;
+//                 }
+//             }else{//SI NO ES NUMERO
+//                 return `El numero ${num} no es número como tal`;
+//                 break;
+//             }
+//         }
+//     }else{
+//         return `No es array`;
+//     }
+//     return `El numero de pares es ${totalpares}`;
+// }
 
 
-console.log(cuentaPares([1,2,3]));
+// console.log(cuentaPares([1,2,3]));
 
 //vale con instaceof o con el metodo isarray
 
@@ -100,4 +100,159 @@ en funciones sencillas y pocas lineas de codigo,lo optimiza */
 // console.log(doblearrow(5));
 
 
+/*M E T O D O S   F O R E A C H, M A P   Y    */
+/*Metodo foreach: permite iterar sobre los elementos
+de array, set y map
 
+No devueñve un nuevo array, no modifica el array original
+No puedo parar el ciclo de iteracion con break, continue
+No renderiza return:
+
+S I N T A X I S:
+array.forEach(funcion_anonima(parametro1: elemento, param2: indice, param3: array original)){
+istrucciones ; }
+elem al q esta accediendo en cada iteracion
+*/
+
+// const desayunos = ["yogur", "petisuis", "rollo canela"];
+// desayunos.forEach(function(elemento){
+//     console.log(elemento); //me visualiza los elementos del array
+// });
+
+// /*con arrowfunction:  */
+// desayunos.forEach(d = console.log(d));
+
+// //foreach en sets
+// const conjunto = new Set(["hola", "adios", "bye"]);
+// conjunto.forEach(function(slaudo){
+//     console.log(saludo);
+// });
+
+// conjunto.forEach(s => console.log(s));
+
+// //foreach en mapa
+// const mapa = new Map([
+//     [1, "hola"],
+//     [2, "adios"],
+//     [2, "bye"]
+// ]);
+
+// mapa.forEach(function(elemento, indice){
+//     console.log(`Indice: ${indice} elemento: ${elemento}`);
+// })
+
+// mapa.forEach((e,i) => console.log(`Indice: ${i} elemento: ${e}`));
+
+
+//array con edades de alumnos, con foreahc realizamos el conteo de +18
+//array con notas y hacemos media
+
+
+
+
+
+const edades = [19, 18, 7, 14, 5, 23, 19];
+let contmayores = 0;
+edades.forEach(function(alumno){
+    if (alumno>=18){
+        contmayores++;
+    }
+})
+console.log(`Numero de mayores de edad: ${contmayores}`)
+
+//con arrow:
+// desayunos.forEach(d => console.log(d));
+
+
+const notas = [9, 8, 7, 6, 5, 4, 3];
+let nota = 0;
+
+let total=0;
+notas.forEach(function(alumno){
+    nota = nota + alumno;
+})
+console.log(`La media es: ${nota/notas.length}`);
+
+
+//como lo hace iria:
+ const edadesn = [16,19, 24,17, 36, 40];
+ function contarMayores(array){
+    let mayores = 0; //si no se inicializa sale como undefined.
+    array.forEach(function(edadn){
+        if(edadn>=18){
+            mayores ++;
+        }
+    });
+    return mayores;
+}
+console.log(contarMayores(edades));
+
+//como hacer el sig:  
+const notasn = [9, 8, 7, 6, 5, 4, 3];
+function median(array){
+   let notasp = 0; //si no se inicializa sale como undefined.
+   array.forEach(function(alumno){
+       notasp = notasp + alumno;
+   });
+   return (notasp/notasn.length);
+}
+console.log(median(notasn)); /*pasar el array */
+
+//arrow function: 
+const edadesi = [16,19, 24,17, 36, 40];
+function contarMayores(array){
+   let mayores = 0; //si no se inicializa sale como undefined.
+   array.forEach( edadi =>{
+       if(edadi>=18){
+           mayores ++;
+       }
+   });
+   return mayores;
+}
+console.log(contarMayores(edades));
+
+
+//LoS MISMOS EJERCICIOS PERO CON MAP
+//Es como el foreach, itera sobre elemn del array, sobre conjuntos
+//lo q hace distinto es q devuelve un nuevo array
+
+/*SINTAXIS:
+const nuevoArrray = array.map(function(elemento, indice, valor){
+    instrucciones
+}) */
+
+
+//consigue nuevo array con elementos pares
+const numeros2 = [1,2,3,4,5,6,7];
+
+const mapa = new Map([
+    [1, "hola"],
+    [2, "adios"],
+    [2, "bye"]
+]);
+
+mapa.forEach(function(elemento, indice){
+    console.log(`Indice: ${indice} elemento: ${elemento}`);
+})
+
+const pares = new Map([
+    [1, 1],
+    [2,2],
+    [3,3],
+    [4,4],
+    [5,5],
+    [6,6],
+    [7,7]
+]);
+
+pares.forEach(function(par, indice){
+    let contpares = [];
+    if(pares %2 ==0){
+        contpares.push(pares);
+    }
+    console.log(contpares);
+})
+
+
+
+//consifue nuevo array con las pañabras de mas de 4 letras
