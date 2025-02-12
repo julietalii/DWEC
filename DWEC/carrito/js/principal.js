227b12dbf363769886e21a3a3c10c864c00594b2
Julieta <jlancellottii23@fpcoruna.afundacion.org>
2025-02-12 10:08:35 +0100

    Commit 10

---
+++ DWEC/carrito/js/principal.js
@@ -0,0 +1,92 @@
+//Referencias a elementos
+//Acceder al icono del carrito
+//Como quiero mostrar el carrito cuando hago click...
+const iconoCarrito = document.getElementById("acarrito") ;
+//CONSEJO: console.log para ver que vamos bien
+//Ahora q tengo referencia, hago evento
+
+iconoCarrito.addEventListener("click",mostrarCarrito);
+//no se le pueden poner parametros al event listener, si no hay q hacer arrow dentro
+
+function mostrarCarrito(){
+    console.log("Entro en mostrar carrito");
+    //saber si hay una clase
+    console.log(carrito.classList.contains("mostrar"));
+    //queremos añadirle la clase
+    if(!carrito.classList.contains("mostrar")){
+        carrito.classList.add("mostrar");
+    }else{
+        carrito.classList.remove("mostrar");
+    }
+}
+//toggle mejor para mostrarcarrito
+
+//en el css del carrito, sale display none, por eso al inicio no se muestra
+//tenemos q pasarlo a display block
+//para eso, accedemos a referencia del elemento, podemos ir con el dom (combinadores css?¿)
+
+//accedemos a div carrito: 
+const carrito = document.getElementById("carrito");
+console.log(carrito); 
+
+//AddEventListner
+//Métodos
+
+//vamos a vaciar carrito:
+//necesitamos referencia a tbody y al boton de vaciar carrito
+//boton de vaciar carrito
+const vaciarCarritoBtn = document.getElementById("vaciar-carrito");
+console.log(vaciarCarritoBtn);
+
+//tbody referencia, usamos queryselector
+const contenidoCarrito = document.querySelector("#lista-carrito tbody");
+console.log(contenidoCarrito);
+
+
+//evento cuando clico em vaciarcarritoboton
+
+vaciarCarritoBtn.addEventListener("click", vaciarCarrito);
+
+//funcion vaciarCarrito:
+
+function vaciarCarrito(){
+    articulosCarrito = [];
+    //vaciamso array de articulos
+    console.log("entro en vaciar carrito");
+    //acceder al innerhtml del contenido del carrito
+    console.log(contenidoCarrito);
+    //quiero vaciarlo
+    contenidoCarrito.innerHTML="";
+    //innerhtml añado texto y etiquetas, vaciamos contenido de tbody
+}
+
+//estructura de datos 
+// intermedia para confirmar añadir al carrito, cantidad etc
+//array!
+
+let articulosCarrito = [];
+
+//botones de añadir curso: 
+btnAnadirCurso = document.querySelectorAll(".info-card a.boton");
+console.log(btnAnadirCurso);
+//addevent: tengo que recorrer el nodelist:
+for(let b of btnAnadirCurso){
+    b.addEventListener("click", anadirCurso);
+}
+
+function anadirCurso(e){
+    console.log("Entro en añadir curso");
+    console.log(e.type);
+    console.log(e.target);
+   /*intentos del ejercicio */
+    a = document.querySelectorAll(".info-card .not(a.boton)");
+    contenidoCarrito.appendChild(a);
+    console.log("pasa por aquí");
+}
+
+//extraer info de evento, parametro e, dos propiedades e.type, evento q se lanza
+//e.target (evento q lanzo el evento )
+//toggle:
+// function toggle(){
+//     carrito.classList.toggle("mostrar");
+// }
